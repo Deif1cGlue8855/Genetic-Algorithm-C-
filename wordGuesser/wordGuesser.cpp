183c3255aa47c613d2f516267bb5ec48514140c6
Deif1cGlue8855 <arloteubner@gmail.com>
2026-01-08 11:03:09 +0000

    Added some more comments

--- wordGuesser/wordGuesser.cpp
+++ wordGuesser/wordGuesser.cpp
@@ -4,6 +4,9 @@
 std::vector<std::string> createBounds(){
     std::vector<std::string> tempList;
     for(int i = 36; i < 127; i++){
+        //Loops through the numbers 36 -> 127
+        //and converts the numbers ti their ASCII
+        //Characters which are store in an array
         char chr = char(i);
         tempList.push_back(std::string(1,chr));
     }
@@ -15,6 +18,11 @@ std::vector<std::string> createBounds(){
 std::vector<float> fitnessFunction(std::vector<DNA> allDNA, std::string target){
     std::string goal = target;
     std::vector<float> allFitness;
+    /*
+    Loops through each letter in the current guess it's looking at and compares it to 
+    the letter in the same place in the goal string, eventually resulting with a percentage 
+    on how accurate the guess was
+    */
 
     for(auto& DNA : allDNA){
         float correct = 0;
@@ -26,28 +34,35 @@ std::vector<float> fitnessFunction(std::vector<DNA> allDNA, std::string target){
     }
     return allFitness;
 }
-
+//Location to be saved at
 std::string fileName = "wordGuessSave.txt";
-std::string target = "Hallo Oma Aus Deutchland";
+//Target string
+std::string target = "dkafgow g8ooq3g ro4qg2igr 892ypq 34rqg ' '    arq k 9 23";
 
 
 int main(){
+    //Initialises RNG so multiple GA's can be used
     initRNG();
 
+    //Gets all the characters the GA will be able to use
     std::vector<std::string> charBounds = createBounds();
-    GA ga(100,target.size(),0.03, 0.4,fileName, charBounds, 0);
+    //Initialises the GA
+    GA ga(100,target.size(),0.03, 0.4,fileName, charBounds, 1);
+    //Creates the first generation
     ga.makeFirstGen();
     int i = 0;
+    //Loops the learning forever
     while(true){
+        //Learning process functions
         ga.setFitness(fitnessFunction(ga.getDNAList(), target));
         ga.sortFitness();
         ga.crossover(4, 20);
         ga.applyMutation();
         ga.changeMutation();
         i++;
+        //Only show the graphics every 1 generation 
         if(i % 1 == 0){
             ga.drawBars(10);
-            //ga.lineGraph();
             ga.showInfo(target.size());
             ga.saveGen();
             ga.printBuffer();
